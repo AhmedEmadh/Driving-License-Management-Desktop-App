7774550687f14fb2a7017f63df3a5056afd52795
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2025-03-05 19:17:46 +0200

    Added Docstring to clsCountry In business Logic Layer

--- Driving-License-Management-BusinessLogic/clsCountry.cs
+++ Driving-License-Management-BusinessLogic/clsCountry.cs
@@ -8,25 +8,40 @@ using System.Threading.Tasks;
 
 namespace Driving_License_Management_BusinessLogic
 {
+    /// <summary>
+    /// Represents a country with an ID and name.
+    /// </summary>
     public class clsCountry
     {
         public int CountryID { get; set; }
         public string CountryName { get; set; }
-
-        public clsCountry() 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="clsCountry"/> class.
+        /// </summary>
+        public clsCountry()
         {
             CountryID = -1;
             CountryName = string.Empty;
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="clsCountry"/> class with the specified ID and name.
+        /// </summary>
+        /// <param name="CountryID">The unique identifier for the country.</param>
+        /// <param name="CountryName">The name of the country.</param>
         private clsCountry(int CountryID, string CountryName)
         {
             this.CountryID = CountryID;
             this.CountryName = CountryName;
         }
+        /// <summary>
+        /// Finds a country by its ID.
+        /// </summary>
+        /// <param name="CountryID">The ID of the country to find.</param>
+        /// <returns>The country with the specified ID, or null if not found.</returns>
         public static clsCountry Find(int CountryID)
         {
             string CountryName = string.Empty;
-            if(clsCountryData.GetCountryInfoByID(CountryID, ref CountryName))
+            if (clsCountryData.GetCountryInfoByID(CountryID, ref CountryName))
             {
                 return new clsCountry(CountryID, CountryName);
             }
@@ -35,18 +50,27 @@ namespace Driving_License_Management_BusinessLogic
                 return null;
             }
         }
+        /// <summary>
+        /// Finds a country by its name.
+        /// </summary>
+        /// <param name="CountryName">The name of the country to find.</param>
+        /// <returns>The country with the specified name, or null if not found.</returns>
         public static clsCountry Find(string CountryName)
         {
             int CountryID = -1;
-            if(clsCountryData.GetCountryInfoByName(CountryName, ref CountryID))
+            if (clsCountryData.GetCountryInfoByName(CountryName, ref CountryID))
             {
-                return new clsCountry(CountryID,CountryName);
+                return new clsCountry(CountryID, CountryName);
             }
             else
             {
                 return null;
             }
         }
+        /// <summary>
+        /// Retrieves a list of all countries.
+        /// </summary>
+        /// <returns>A DataTable containing all countries.</returns>
         public static DataTable GetAllCountries()
         {
             return clsCountryData.GetAllCountries();
